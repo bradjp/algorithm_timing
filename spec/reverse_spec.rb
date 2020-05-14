0ec2f7af134b64d1cfae50293a2f761afefc7f4c
Brad Phillips <bjrp@protonmail.com>
2020-05-14 21:49:25 +0100

    Reverses a 3-item array

--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -15,5 +15,8 @@ describe Reverse do
     it 'reverses [0,1] to [1,0]' do
       expect(subject.reverse_two([0,1])).to eq([1,0])
     end
+    it 'reverses [0,1,2] to [2,1,0]' do
+      expect(subject.reverse_two([0,1])).to eq([1,0])
+    end
   end
 end
