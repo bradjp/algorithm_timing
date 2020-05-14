d8f23301c9ca87a856b3bae9062dedd649f0a040
Brad Phillips <bjrp@protonmail.com>
2020-05-14 21:54:05 +0100

    Reverses a 10-item array

--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -18,5 +18,8 @@ describe Reverse do
     it 'reverses [0,1,2] to [2,1,0]' do
       expect(subject.reverse_two([0,1])).to eq([1,0])
     end
+    it 'reverses [0..10] to [10..0]' do
+      expect(subject.reverse_two((0..10).to_a)).to eq([10,9,8,7,6,5,4,3,2,1,0])
+    end
   end
 end
