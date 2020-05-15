3287f83ba87cd447c7e8ec192ad262b7299af82d
Brad Phillips <bjrp@protonmail.com>
2020-05-15 22:15:26 +0100

    Timing framework in progress

--- lib/timer.rb
+++ lib/timer.rb
@@ -2,7 +2,20 @@ require_relative 'reverse'
 
 def code_timer(method, input)
   x = Reverse.new
+
+  five = [0,2,3,4]
+  ten = [98,98]
+  fifteen = []
+  twenty = []
+  twenty_five = []
+  
+
   start = Time.now
-  x.send(method, input)
-  Time.now - start
+  x.send(method, five)
+  five_k_time = Time.now - start
+
+  p ten
+  p five
+  puts "Time\n\n----\n\nFive-thousand-item array: #{five_k_time}"
+
 end
