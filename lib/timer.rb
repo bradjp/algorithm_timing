bfe7ba0a01cc604d71e5878554547375f33ebdb2
Brad Phillips <bjrp@protonmail.com>
2020-05-16 22:31:28 +0100

    Prints times for 5,10,15,20 and 25 K arrays

--- lib/timer.rb
+++ lib/timer.rb
@@ -4,18 +4,42 @@ def code_timer(method, input)
   x = Reverse.new
 
   five_k = []
-  ten = [98,98]
-  fifteen = []
-  twenty = []
-  twenty_five = []
+  ten_k = []
+  fifteen_k = []
+  twenty_k = []
+  twenty_five_k = []
 
   5000.times { five_k << input }
-  p five_k
+  10000.times { ten_k << input }
+  15000.times { fifteen_k << input }
+  20000.times { twenty_k << input }
+  25000.times { twenty_five_k << input }
 
   start = Time.now
   x.send(method, five_k)
   five_k_time = Time.now - start
+  
+  start = Time.now
+  x.send(method, ten_k)
+  ten_k_time = Time.now - start
+  
+  start = Time.now
+  x.send(method, fifteen_k)
+  fifteen_k_time = Time.now - start
+  
+  start = Time.now
+  x.send(method, twenty_k)
+  twenty_k_time = Time.now - start
+  
+  start = Time.now
+  x.send(method, twenty_five_k)
+  twenty_five_k_time = Time.now - start
 
-  puts "Time\n\n----\n\nFive-thousand-item array: #{five_k_time}"
+  puts "Time\n\n----\n
+  \n5,000 item array: #{five_k_time}
+  \n10,000 item array: #{ten_k_time}
+  \n15,000 item array: #{fifteen_k_time}
+  \n20,000 item array: #{twenty_k_time}
+  \n25,000 item array: #{twenty_five_k_time}"
 
 end
