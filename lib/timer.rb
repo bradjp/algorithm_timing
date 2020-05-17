e44d42fa92f78e4b6613b9169962c204a0324d6b
Brad Phillips <bjrp@protonmail.com>
2020-05-17 17:53:02 +0100

    Refactoring -  timer.rb

--- lib/timer.rb
+++ lib/timer.rb
@@ -2,11 +2,7 @@ require_relative 'reverse'
 
 def code_timer(method, input)
 
-  five_k = []
-  ten_k = []
-  fifteen_k = []
-  twenty_k = []
-  twenty_five_k = []
+  five_k, ten_k, fifteen_k, twenty_k, twenty_five_k = [],[],[],[],[]
 
   5000.times { five_k << input }
   10000.times { ten_k << input }
@@ -23,13 +19,15 @@ def code_timer(method, input)
   twenty_k_time = run_timer(method, twenty_k)
 
   twenty_five_k_time = run_timer(method, twenty_five_k)
-  
-  puts "Time\n\n----\n
-  \n5,000 item array: #{five_k_time}
-  \n10,000 item array: #{ten_k_time}
-  \n15,000 item array: #{fifteen_k_time}
-  \n20,000 item array: #{twenty_k_time}
-  \n25,000 item array: #{twenty_five_k_time}"
+
+  def print_times(*times)
+    puts "Time\n\n----\n
+    \n5,000 item array: #{five_k_time}
+    \n10,000 item array: #{ten_k_time}
+    \n15,000 item array: #{fifteen_k_time}
+    \n20,000 item array: #{twenty_k_time}
+    \n25,000 item array: #{twenty_five_k_time}"
+  end
 
 end
 
