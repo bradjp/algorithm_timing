38008137bf635f4ab03c183ceb0850d0e2b1ef31
Brad Phillips <bjrp@protonmail.com>
2020-05-17 20:35:53 +0100

    Rubocop installed; new timing method in progress

---
+++ lib/average_timer.rb
@@ -0,0 +1,19 @@
+require_relative 'reverse'
+
+def code_timer(method, array_size, no_of_tests)
+
+  arr = []
+
+  array_size.times { arr << 0 }
+
+   run_timer(method, arr)
+  
+
+end
+
+def run_timer(method, arr)
+  x = Reverse.new
+  start = Time.now
+  x.send(method, arr)
+  output = Time.now - start
+end
