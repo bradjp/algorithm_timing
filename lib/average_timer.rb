376d77c3f9ea3459a04d506ebf84e2424bdae5d0
Brad Phillips <bjrp@protonmail.com>
2020-05-17 21:22:12 +0100

    average_timer.rb working

--- lib/average_timer.rb
+++ lib/average_timer.rb
@@ -1,16 +1,28 @@
 require_relative 'reverse'
 
-def code_timer(method, array_size, _no_of_tests)
+def code_timer(method, array_size, no_of_runs)
   arr = []
+  test_results = []
 
   array_size.times { arr << 0 }
 
-  run_timer(method, arr)
+  no_of_runs.times { test_results << run_timer(method, arr) }
+
+  print_results(test_results)
+
 end
 
 def run_timer(method, arr)
   x = Reverse.new
   start = Time.now
   x.send(method, arr)
-  output = Time.now - start
+  Time.now - start
+end
+
+def average_time(results)
+  results.inject(:+) / results.length 
+end
+
+def print_results(results)
+  puts "Results\n-------\n\nMinimum time: #{results.min}\n\nMaximum time: #{results.max}\n\nAverage time: #{average_time(results)}"
 end
