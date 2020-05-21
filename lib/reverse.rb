c25c8582e6a6356d10ccaec7747b83a7a470ecb2
Brad Phillips <bjrp@protonmail.com>
2020-05-21 21:19:50 +0100

    Reverse 3 passes all tests

--- lib/reverse.rb
+++ lib/reverse.rb
@@ -11,7 +11,23 @@ class Reverse
   end
 
   def reverse_three(arr)
-    arr
+
+    new_array = []
+
+    final_array = []
+
+    arr.each_with_index do |item,index|
+      new_array << index
+    end
+    
+    new_array.sort! { |low, high| high <=> low }
+
+    zipped_array = arr.zip(new_array)
+
+    final_array = zipped_array.sort! { |el| el[1] }
+
+    final_array.map { |el| el[0] }
+
   end
 
 
--- spec/reverse_spec.rb
+++ spec/reverse_spec.rb
@@ -14,7 +14,7 @@ describe Reverse do
       expect(subject.reverse_two([0, 1])).to eq([1, 0])
     end
     it 'reverses [0,1,2] to [2,1,0]' do
-      expect(subject.reverse_two([0, 1])).to eq([1, 0])
+      expect(subject.reverse_two([0, 1, 2])).to eq([2, 1, 0])
     end
     it 'reverses [0..10] to [10..0]' do
       expect(subject.reverse_two((0..10).to_a)).to eq([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
@@ -24,17 +24,17 @@ describe Reverse do
     it 'returns an empty array' do
       expect(subject.reverse_three([])).to eq([])
     end
-    # it 'returns an single item array' do
-    #   expect(subject.reverse_two([2])).to eq([2])
-    # end
-    # it 'reverses [0,1] to [1,0]' do
-    #   expect(subject.reverse_two([0, 1])).to eq([1, 0])
-    # end
-    # it 'reverses [0,1,2] to [2,1,0]' do
-    #   expect(subject.reverse_two([0, 1])).to eq([1, 0])
-    # end
-    # it 'reverses [0..10] to [10..0]' do
-    #   expect(subject.reverse_two((0..10).to_a)).to eq([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
-    # end
+    it 'returns an single item array' do
+      expect(subject.reverse_three([2])).to eq([2])
+    end
+    it 'reverses [0,1] to [1,0]' do
+      expect(subject.reverse_three([0, 1])).to eq([1, 0])
+    end
+    it 'reverses [0,1,2] to [2,1,0]' do
+      expect(subject.reverse_two([0, 1])).to eq([1, 0])
+    end
+    it 'reverses [0..10] to [10..0]' do
+      expect(subject.reverse_two((0..10).to_a)).to eq([10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0])
+    end
   end
 end
