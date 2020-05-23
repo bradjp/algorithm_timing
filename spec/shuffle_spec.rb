947515cc3535701ac6ac6316194a29e06e7fb515
Brad Phillips <bjrp@protonmail.com>
2020-05-23 16:47:23 +0100

    Correct test error

--- spec/shuffle_spec.rb
+++ spec/shuffle_spec.rb
@@ -8,7 +8,7 @@ describe Shuffle do
     it 'returns an empty array' do
       expect(subject.shuffle_two([])).to eq([])
     end
-    it 'returns an empty array' do
+    it 'shuffles an array' do
       pre_shuffle = ['a', 'b', 'c', 'd', 'e']
       post_shuffle = subject.shuffle_two(['a', 'b', 'c', 'd', 'e'])
       expect(subject.shuffle_two(['a','b','c','d', 'e'])).not_to eq([pre_shuffle])
@@ -20,12 +20,12 @@ describe Shuffle do
     it 'returns an empty array' do
       expect(subject.shuffle_three([])).to eq([])
     end
-    # it 'returns an empty array' do
-    #   pre_shuffle = ['a', 'b', 'c', 'd', 'e']
-    #   post_shuffle = subject.shuffle_three(['a', 'b', 'c', 'd', 'e'])
-    #   expect(subject.shuffle_three(['a','b','c','d', 'e'])).not_to eq([pre_shuffle])
-    #   expect(pre_shuffle - post_shuffle).to eq([])
-    # end
+    it 'shuffles an array' do
+      pre_shuffle = ['a', 'b', 'c', 'd', 'e']
+      post_shuffle = subject.shuffle_three(['a', 'b', 'c', 'd', 'e'])
+      expect(subject.shuffle_three(['a','b','c','d', 'e'])).not_to eq([pre_shuffle])
+      expect(pre_shuffle - post_shuffle).to eq([])
+    end
   end
   
 end
