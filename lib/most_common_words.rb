9153993bab1df81daf7025bf84f4958816af74ce
Brad Phillips <bjrp@protonmail.com>
2020-05-26 21:38:55 +0100

    Rubocop corrections

--- lib/most_common_words.rb
+++ lib/most_common_words.rb
@@ -1,5 +1,4 @@
 class Most_common_words
-
   def most_common_words(arr)
     hash = {}
 
@@ -11,9 +10,9 @@ class Most_common_words
       end
     end
 
-    x = hash.sort_by { |k,v| v }.to_a
+    x = hash.sort_by { |_k, v| v }.to_a
 
-    if x.length < 2 
+    if x.length < 2
       nil
     elsif x.length == 2
       [x[-1][0], x[-2][0]].sort
--- spec/most_common_words_spec.rb
+++ spec/most_common_words_spec.rb
@@ -1,7 +1,6 @@
 require 'most_common_words'
 
 describe Most_common_words do
-
   subject(:double) { described_class.new }
 
   describe '#most_common_words' do
@@ -12,14 +11,13 @@ describe Most_common_words do
       expect(subject.most_common_words(['Hello'])).to eq(nil)
     end
     it 'returns both items of a two-item array' do
-      expect(subject.most_common_words(['Hello', 'world'])).to eq(['Hello', 'world'])
+      expect(subject.most_common_words(%w[Hello world])).to eq(%w[Hello world])
     end
     it 'returns the two most common items of an array' do
-      expect(subject.most_common_words(['Hello', 'world', 'Hello', 'world', 'single'])).to eq(['Hello', 'world'])
+      expect(subject.most_common_words(%w[Hello world Hello world single])).to eq(%w[Hello world])
     end
     it 'returns the two most common items of an array' do
-      expect(subject.most_common_words(['Hello', 'world', 'Hello', 'world', 'Howdy','Howdy'])).to eq(['Howdy', 'world'])
+      expect(subject.most_common_words(%w[Hello world Hello world Howdy Howdy])).to eq(%w[Howdy world])
     end
   end
-
 end
